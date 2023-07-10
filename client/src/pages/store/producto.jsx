71ae0967ed32302a3ea6e3a5ffdd2d23179080a4
stivcs <stivencsoto@gmail.com>
2023-07-10 00:29:28 -0500

    v2 document

--- client/src/pages/store/producto.jsx
+++ client/src/pages/store/producto.jsx
@@ -2,28 +2,35 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Producto(props) {
-    const { name, precio, img1, img2, img3 } = props.info;
+    // Desestructurar las propiedades del producto
+    const { name, price, img1, img2, img3 , idProduct} = props.info;
+    // Obtener la función de navegación
     const navigate = useNavigate();
 
+    // Función para navegar al producto
     const navigateProduct = () => {
-        navigate(`/product?name=${name}`);
+        navigate(`/product?id=${idProduct}`);
     };
+
     return (
-        <div className="producto">
+        <div className="product">
             <div className="slide-var">
                 <ul>
+                    {/* Renderizar las imágenes del producto */}
                     <li><img id="img" src={img1} alt={name} /></li>
                     <li><img id="img" src={img2} alt={name} /></li>
                     <li><img id="img" src={img3} alt={name} /></li>
                 </ul>
             </div>
-            <div className="descripcion">
+            <div className="description">
+                {/* Renderizar el nombre y el precio del producto */}
                 <p>{name}</p>
-                <p>{precio}</p>
-                <button class = "btn-edeptec"  type="button" onClick={navigateProduct} > Ver </button>
+                <p>{price}</p>
+                {/* Renderizar el botón "Ver" con el evento onClick para navegar al producto */}
+                <button className="botton" type="button" onClick={navigateProduct}>Ver</button>
             </div>
         </div>
-    )
+    );
 }
 
 export default Producto;
